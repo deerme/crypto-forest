cec3d5a41b260291e337be29d611d72b2f38e81d
luisaoisdead <luis_idc@hotmail.com>
2020-12-23 12:13:56 -0300

    Se genera la funcion tradeTest()
    La funcion nos da como respuesta 200.
    :) la misma se encuentra funcionando.
    Se comienza a utilizar la base + endpoint en todas las funciones.

--- binance.py
+++ binance.py
@@ -2,6 +2,9 @@
 Created on Mon Dec  7 10:50:00 2020
 
 @author: nicopacheco121
+
+fuente de referencia:
+    https://binance-docs.github.io/apidocs/spot/en/#introduction
 """
 
 import requests
@@ -18,7 +21,7 @@ except:
 
 
 """ WALLET ENDPOINTS"""
-url = 'https://api.binance.com/'
+base = 'https://api.binance.com'
 
 
 # SYSTEM STATUS
@@ -33,7 +36,8 @@ def system_status():
 # DAILY ACCOUNT SNAPSHOT (RESUMEN DIARIO DE LA CUENTA)
     
 def account_snapshot(key,secret):
-    url ='https://api.binance.com/sapi/v1/accountSnapshot'
+    endpoint='/sapi/v1/accountSnapshot'
+    url=base+endpoint 
     ts=horaservidor()
     recvWindow = 5000
 
@@ -52,8 +56,26 @@ def account_snapshot(key,secret):
 
     return js
 
+# ENVIO DE OPERARION DE PRUEBA
+def tradeTest(key,secret):
+    endpoint='/api/v3/order/test'
+    url=base+endpoint    
+    ts=horaservidor()
+    params = {'symbol':'BTCUSDT','quantity':5, 'side':'SELL','type':'MARKET','timestamp':ts}
+    # signature
+    h = urlencode(params)
+    b = bytearray()
+    b.extend(secret.encode())
+    signature = hmac.new(b, msg=h.encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
+    params['signature'] = signature
+
+    headers = {'X-MBX-APIKEY': key}
+    r = requests.post(url=url, params=params, headers=headers, verify=True)
+    return r
+
+
+
 def horaservidor():
-    base='https://api.binance.com'
     endpoint='/api/v3/time'
     url=base+endpoint
     params = {}
@@ -61,11 +83,12 @@ def horaservidor():
     js = r.json()
     return js['serverTime']
 
+
 #Caso de Uso.
-
-
-
 if __name__ == '__main__':
-
+    print('Estado de la cuenta:')
     data_cuenta = account_snapshot(key= keys.BINANCE_KEY, secret=keys.BINANCE_SECRET)
     print(data_cuenta)
+    pruebaTrade=tradeTest(key= keys.BINANCE_KEY, secret=keys.BINANCE_SECRET)
+    print(pruebaTrade)
+
